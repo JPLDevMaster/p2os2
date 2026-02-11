ee8a1d2658ad6356bbf73843c89c62f702ff12bf
JPLDevMaster <joao.penha.lopes@tecnico.ulisboa.pt>
2026-02-11 21:54:47 +0000

    [feat]: Updated the driver launcher to accept 'port' as an external parameters for MobileSim testing.

--- p2os_bringup/launch/p2os_driver_launch.py
+++ p2os_bringup/launch/p2os_driver_launch.py
@@ -1,8 +1,20 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # Create the LaunchConfiguration variable to capture the value.
+    port_config = LaunchConfiguration('port')
+
     return LaunchDescription([
+        # Declare the argument so we can use 'port:=...' in the terminal.
+        DeclareLaunchArgument(
+            'port',
+            default_value='/dev/ttyUSB0',
+            description='Serial port for the p2os_driver'
+        ),
+
         Node(
             package='p2os_driver',
             executable='p2os_driver',
@@ -12,7 +24,7 @@ def generate_launch_description():
             ],
             parameters=[
                 {'use_sonar': False},
-                {'port': '/dev/ttyUSB0'} #make sure this is the right port
+                {'port': port_config}  # Use the captured value.
             ],
             arguments=['--ros-args', '--log-level', 'INFO']
         ),            
